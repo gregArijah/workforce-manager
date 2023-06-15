0225e20f248e398aee54e9f52a21097ba168bc3b
Greg Griffith <griffith_g@hotmail.com>
2023-06-15 19:42:28 -0400

    updated route handler for employee

---
+++ app/api/employee.ts
@@ -0,0 +1,73 @@
+import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+  const { id } = req.query;
+
+  switch (method) {
+    case 'GET':
+      if (id) {
+        try {
+          const employee = await prisma.employee.findUnique({
+            where: { id },
+            include: { department: true, company: true, timeCard: true },
+          });
+          res.status(200).json(employee);
+        } catch (error) {
+          res.status(500).json({ error: 'Error retrieving the employee.' });
+        }
+      } else {
+        try {
+          const employees = await prisma.employee.findMany();
+          res.status(200).json(employees);
+        } catch (error) {
+          res.status(500).json({ error: 'Error retrieving employees.' });
+        }
+      }
+      break;
+
+    case 'POST':
+      const { name, code, departmentId, companyId } = req.body;
+      try {
+        const employee = await prisma.employee.create({
+          data: { name, code, departmentId, companyId },
+        });
+        res.status(201).json(employee);
+      } catch (error) {
+        res.status(500).json({ error: 'Error creating the employee.' });
+      }
+      break;
+
+    case 'PUT':
+      try {
+        const { name, code, departmentId, companyId } = req.body;
+        const updatedEmployee = await prisma.employee.update({
+          where: { id },
+          data: { name, code, departmentId, companyId },
+        });
+        res.status(200).json(updatedEmployee);
+      } catch (error) {
+        res.status(500).json({ error: 'Error updating the employee.' });
+      }
+      break;
+
+    case 'DELETE':
+      try {
+        const deletedEmployee = await prisma.employee.delete({
+          where: { id },
+        });
+        res.status(200).json(deletedEmployee);
+      } catch (error) {
+        res.status(500).json({ error: 'Error deleting the employee.' });
+      }
+      break;
+
+    default:
+      res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
+}
