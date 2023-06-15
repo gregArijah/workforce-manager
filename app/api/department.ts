0f9ff1e645dae848dae5c7d3b194f5ef552830e2
Greg Griffith <griffith_g@hotmail.com>
2023-06-15 19:28:21 -0400

    added route handler for department

---
+++ app/api/department.ts
@@ -0,0 +1,71 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { method } = req;
+    const { id } = req.query;
+
+    switch (method) {
+        case "GET":
+            if (id) {
+                try {
+                    const department = await prisma.department.findUnique({
+                        where: { id },
+                        include: { employees: true },
+                    });
+                    res.status(200).json(department);
+                } catch (error) {
+                    res.status(500).json({ error: "Error retrieving the department." });
+                }
+            }
+            break;
+
+        case "POST":
+            const { name, code, password, adminPassword } = req.body;
+            try {
+                const department = await prisma.department.create({
+                    data: { name, code, password, adminPassword },
+                });
+                res.status(201).json(department);
+            }
+            catch (error) {
+                res.status(500).json({ error: "Error creating the department." });
+            }
+            break;
+
+        case "PUT":
+            try {
+                const { name, code, password, adminPassword } = req.body;
+                const updatedDepartment = await prisma.department.update({
+                    where: { id },
+                    data: { name, code, password, adminPassword },
+                });
+                res.status(200).json(updatedDepartment);
+            }
+            catch (error) {
+                res.status(500).json({ error: "Error updating the department." });
+            }
+            break;
+
+        case "DELETE":
+            try {
+                const deletedDepartment = await prisma.department.delete({
+                    where: { id },
+                });
+                res.status(200).json(deletedDepartment);
+            }
+            catch (error) {
+                res.status(500).json({ error: "Error deleting the department." });
+            }
+            break;
+
+        default:
+            res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+            res.status(405).end(`Method ${method} Not Allowed`);
+            break;
+        }
+
+
+}
