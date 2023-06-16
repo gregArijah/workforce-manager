aab1a32db70096e7e02019844df6b80f6fb8a7f5
Greg Griffith <griffith_g@hotmail.com>
2023-06-15 20:03:04 -0400

    added prisma library and updated route handlers

--- app/api/timecard.ts
+++ app/api/timecard.ts
@@ -1,7 +1,6 @@
-import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../lib/prisma';
 
-const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
