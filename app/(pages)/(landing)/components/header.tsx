0e1da3a403f5727369d7de1859014f6d7badd997
Greg Griffith <griffith_g@hotmail.com>
2023-07-01 10:34:00 -0400

    updated file structure and landing page

---
+++ app/(pages)/(landing)/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+
+  return (
+      <header className='text-left'>
+        <h1>Header goes here: logo, name, and slogan</h1>
+        <p>WORKFORCE MANAGER</p>
+      </header> 
+  )
+} 
