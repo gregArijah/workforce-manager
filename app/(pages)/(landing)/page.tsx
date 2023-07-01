9e0654a194de344a3e4f41f4af7da8e23e8beccc
Greg Griffith <griffith_g@hotmail.com>
2023-07-01 10:47:44 -0400

    header logo linked to home page

--- app/(pages)/(landing)/page.tsx
+++ app/(pages)/(landing)/page.tsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/navbar";
-import Header from "./components/header"	;
+import Header from "../components/header"	;
 import Coolstuff from "./components/coolstuff";
 
 export default async function Landing() {
--- app/(pages)/components/header.tsx
+++ app/(pages)/components/header.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
+
 export default function Header() {
 
   return (
       <header className='text-left'>
         <h1>Header goes here: logo, name, and slogan</h1>
-        <p>WORKFORCE MANAGER</p>
+        <Link href="/"  className= 'text-red-700'>
+            <p>WORKFORCE MANAGER</p>
+        </Link>
       </header> 
   )
 } 
