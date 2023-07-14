42aba3c41f4022a2f79ae8d6ddf4fa069dd30f8b
Greg Griffith <griffith_g@hotmail.com>
2023-07-13 22:00:02 -0400

    prepare landing for info slides

--- app/(pages)/(landing)/page.tsx
+++ app/(pages)/(landing)/page.tsx
@@ -1,13 +1,17 @@
 import Navbar from "./_components/navbar";
 import Header from "../_components/header"	;
 import Coolstuff from "./_components/coolstuff";
+import Slides from "./_components/slides";
 
 export default function Landing(): JSX.Element {
  
   return (
-    <div className='h-screen p-6'>  
-      <Navbar />
-      <Header />
+    <div className='h-screen p-6'>     
+      <div className="flex justify-between items-center">
+        <Header />
+        <Navbar />      
+      </div>
+      <Slides />
       <Coolstuff />    
     </div>
   )
