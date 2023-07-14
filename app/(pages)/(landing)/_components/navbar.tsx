8e72d4bc9462b2a5003de1ef2c66697f473cfd9f
Greg Griffith <griffith_g@hotmail.com>
2023-07-14 19:46:54 -0400

    modified layout file to pass session to children

--- app/(pages)/(landing)/_components/navbar.tsx
+++ app/(pages)/(landing)/_components/navbar.tsx
@@ -1,10 +1,12 @@
-'use client';
-
 import Link from "next/link";
+import { signIn, signOut, useSession } from 'next-auth/react'
+import { getServerSession } from "next-auth";
+import { getToken } from "next-auth/jwt";
+
 
 
 export default function Navbar() {
- 
+  
   return (
       <nav className='text-right'>
         <Link href='/api/auth/signin'><button className='bg-blue-600 h-8 w-24 rounded'>Login</button></Link>           
