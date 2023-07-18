4e517adf91532eea4f5fc0ee05f43b61f9e016b2
Greg Griffith <griffith_g@hotmail.com>
2023-07-18 03:24:57 -0400

    extracted token into middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,30 @@
 
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { getServerSession } from 'next-auth/next'
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
-import { getSession } from 'next-auth/react'
-import { useSession } from "next-auth/react";
+import { getToken } from 'next-auth/jwt'
+// interface Session {
+//   user: {
+//     name: string;
+//     sub: string;
+//     userRole: string;
+//     iat: number;
+//     exp: number;
+//     jti: string;
+//   };
+// }
 
-export async function middleware(request: NextRequest) {
-  
-    //const session = await getServerSession(authOptions); 
-  
 
-  // if (request.nextUrl.pathname.startsWith('/admin')) {
-  //   return NextResponse.redirect(new URL('/admin', request.url))
-  // }
- 
+export async function middleware(request: NextRequest, response: NextResponse){
+    const token = await getToken({ req: request })
+    console.log("middleware: token",token?.userRole);
+    console.log("middleware: token2222", JSON.stringify(token));
+    
+    const cookies :String|null = request.headers.get('cookie') ;
+    
+    if (request.nextUrl.pathname.startsWith('/punches')) {
+        console.log("middleware: punches",cookies);	
+    }
+
   if (request.nextUrl.pathname.startsWith('/dashboard')) {
     return NextResponse.rewrite(new URL('/dashboard/user', request.url))
   }
