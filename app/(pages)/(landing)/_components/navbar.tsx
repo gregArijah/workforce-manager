6013fc380c7de0253567328edcb7ba3f251ac360
Greg Griffith <griffith_g@hotmail.com>
2023-07-20 00:22:51 -0400

    added callback ot signout and signin)

--- app/(pages)/(landing)/_components/navbar.tsx
+++ app/(pages)/(landing)/_components/navbar.tsx
@@ -1,17 +1,21 @@
-
+'use client'
 import Link from "next/link";
 import SessionInfo from "@/app/lib/clientSession";
+import { signIn, signOut } from "next-auth/react";
 
+
+const login = () => signIn(undefined,{ callbackUrl:'/admin'})
+const logout = () => signOut({ callbackUrl:'/'})
 export default function Navbar() {
 
-  //const {session, loading} = SessionInfo();
-  let session = true, loading = false;  
-  return (  
-        <nav className='text-right'>
-            {session && <Link href='/api/auth/signout'><button className='bg-blue-600 h-8 w-24 rounded'>Logout</button></Link>}
-            {loading && <button className='bg-blue-600 h-8 w-24 rounded'>Verifying...</button>}
-            {!session && !loading && <Link href='/api/auth/signin'><button className='bg-blue-600 h-8 w-24 rounded'>Login</button></Link>}
-        </nav>
-    )
+    const {session, loading} = SessionInfo();
+    
+    return (  
+            <nav className='text-right'>
+                {session && <button onClick={logout} className='bg-blue-600 h-8 w-24 rounded'>Logout</button>}
+                {loading && <button className='bg-blue-600 h-8 w-24 rounded'>Verifying...</button>}
+                {!session && !loading && <button onClick={login} className='bg-blue-600 h-8 w-24 rounded'>Login</button>}
+            </nav>
+        )
 }
 
