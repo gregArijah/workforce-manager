1a140597dd35ddb0c9c2191bc83b1bc6cc483dc3
Greg Griffith <griffith_g@hotmail.com>
2023-07-31 00:31:05 -0400

    added main components in departmen admin pagges

---
+++ app/(pages)/admin/departments/_components/main.tsx
@@ -0,0 +1,34 @@
+interface MainProps {
+    departments: { name: string; code: string }[];
+    }
+
+export default function Main({ departments }: MainProps){
+    return (
+        
+        <div>
+            
+        <div className="border p-4 mb-4">
+          <button className="bg-green-500 text-white px-4 py-2 rounded">Add New</button>
+        </div>
+        {/* Render each department item */}
+        {departments.map((department, index) => (
+          <div key={index} className="border p-4 flex space-x-3">
+            <h2 className="font-bold">{department.name}</h2>
+            <h3 className="text-gray-500">{department.code}</h3>
+
+            <div className="flex mt-2">
+              <button className="mr-2 bg-blue-500 text-white px-4 py-2 rounded">
+                View
+              </button>
+              <button className="mr-2 bg-green-500 text-white px-4 py-2 rounded">
+                Edit
+              </button>
+              <button className="bg-red-500 text-white px-4 py-2 rounded">
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+        </div>
+    )
+}
