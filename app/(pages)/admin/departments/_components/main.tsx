cb6d0cf2045433ecb3f64af2165bb75a98458629
Greg Griffith <griffith_g@hotmail.com>
2023-08-01 19:50:53 -0400

    add delete department function

--- app/(pages)/admin/departments/_components/main.tsx
+++ app/(pages)/admin/departments/_components/main.tsx
@@ -1,11 +1,46 @@
 import Link from "next/link";
 
+
+
+const deleteDepartment = async (department:String) => {
+  
+  const api = `/api/department/?departmentId=${department}`;
+
+  const res = await fetch(api, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const json = await res.json();
+  return json;
+};
+
 interface MainProps {
     departments: { name: string; code: string }[];
     setView: (view: any) => void;
     }
 
 export default function Main({ departments, setView }: MainProps){
+    function handleEdit(){
+        setView('edit');
+    }
+
+    async function handleDelete(department:any){
+       // setView('delete');
+       console.log(department);
+       const isConfirm:Boolean = confirm("Are you sure you want to delete this department?");
+       try{
+          console.log(isConfirm);
+          if (isConfirm){
+              await deleteDepartment(department.id);
+              console.log("deleted");
+          }
+        }catch(err){
+            console.log(err);
+        }
+
+    }
 
     function handleAdd(){
         setView('add');
@@ -30,10 +65,16 @@ export default function Main({ departments, setView }: MainProps){
             <h3 className="text-gray-500">{department.code}</h3>
 
             <div className="flex mt-2">
-              <button onClick={handleView}className="mr-2 bg-blue-500 text-white px-4 py-2 rounded">
-                View
-              </button>
-              
+                <button onClick={handleEdit} className="mr-2 bg-green-500 text-white px-4 py-2 rounded">
+                    Edit
+                  </button>
+                  <button onClick={()=>handleDelete(department)} className="bg-red-500 text-white px-4 py-2 rounded">
+                    Delete
+                  </button>
+                  {/* <button onClick={handleView}className="mr-2 bg-blue-500 text-white px-4 py-2 rounded">
+                    View
+                  </button> */}
+                  
             </div>
           </div>
         ))}
--- app/(pages)/admin/departments/_components/view.tsx
+++ app/(pages)/admin/departments/_components/view.tsx
@@ -11,12 +11,7 @@ export default function ViewDept ({setView, setDepartments, departments}: DeptPr
           <button onClick={()=>setView('main')} className="bg-blue-500 text-white px-4 py-2 rounded">Back</button>
         </div>
             <h1> View Department Page</h1>
-            <button className="mr-2 bg-green-500 text-white px-4 py-2 rounded">
-                Edit
-              </button>
-              <button className="bg-red-500 text-white px-4 py-2 rounded">
-                Delete
-              </button>
+          
             <p>Dept name , code, #of employees</p>
             <p>list of employees, employeecode</p>
             <p></p>
