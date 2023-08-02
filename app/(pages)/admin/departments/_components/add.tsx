83bd8bc53d244dd179a08ebf45aef835ee9623f8
Greg Griffith <griffith_g@hotmail.com>
2023-08-01 20:32:07 -0400

    update add employee ui

--- app/(pages)/admin/departments/_components/add.tsx
+++ app/(pages)/admin/departments/_components/add.tsx
@@ -1,3 +1,9 @@
+//on this page we want to display a form in order to add a new department
+//the form shouls ask for the name and code of the department
+//the form should have a submit button
+
+
+
 interface DeptProps {
     setView: (view: any) => void;
     setDepartments: (dept: any) => void;
@@ -10,10 +16,21 @@ export default function AddDept ({setView, setDepartments, departments}: DeptPro
               <div className="border p-4 mb-4">
           <button onClick={()=>setView('main')} className="bg-blue-500 text-white px-4 py-2 rounded">Back</button>
         </div>
-            <h1> Add Department Page</h1>
-            <p>some forms and stuff to add new department</p>
-            <p>dept name, code, etc</p>
-            <p>cancel or submit buttons</p>
+            <h1 className="font-bold pb-2"> Add a new department</h1>
+        
+            <div>
+                <form className="flex flex-col space-y-2">
+                    <label>
+                        Department Name:
+                        <input type="text" name="name" />
+                    </label>
+                    <label>
+                        Department Code :
+                        <input type="text" name="code" />
+                    </label>
+                    <button type="submit"  className="bg-blue-300 p-4 rounded w-24">submit</button>
+                </form>
+            </div>
         </div>
     )
 
