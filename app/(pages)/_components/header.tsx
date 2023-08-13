04277bae6fdd019799566514927d6a8e0471131d
Greg Griffith <griffith_g@hotmail.com>
2023-08-13 10:44:16 -0400

    editing header spacing

--- app/(pages)/_components/header.tsx
+++ app/(pages)/_components/header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Header() {
 
   return (
-      <header className='text-left pb-2'>
+      <header className='text-left pb-6'>
         <Link href="/"  className= 'text-red-700'>
             <Image src='/veleron3.png' width={250} height={100} alt='logo'></Image>
         </Link>
