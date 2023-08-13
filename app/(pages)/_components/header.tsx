1281480c211cf97208a25055ff750a2a50c6e6c0
Greg Griffith <griffith_g@hotmail.com>
2023-08-13 10:15:50 -0400

    added logo

--- app/(pages)/_components/header.tsx
+++ app/(pages)/_components/header.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Header() {
 
   return (
-      <header className='text-left'>
+      <header className='text-left pb-2'>
         <Link href="/"  className= 'text-red-700'>
-            <p className='text-lg'>WORKFORCE MANAGER  **LOGO**</p>
-            <p>slogan text</p>
+            <Image src='/veleron3.png' width={250} height={100} alt='logo'></Image>
         </Link>
+       
       </header> 
   )
 } 
